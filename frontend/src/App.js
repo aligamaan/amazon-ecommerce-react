63b3c15fdca4440d41bc97bc2bcdbd7e8797084e
ali gamaan <aligamaan@gmail.com>
2022-08-17 18:54:10 +0300

    Amazon Ecommerce website

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">Amazon</a>
+      </header>
+      <main>List of Products</main>
+    </div>
+  );
+}
+
+export default App;
